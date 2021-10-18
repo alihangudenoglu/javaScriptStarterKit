12cab5a3c558a7460feb65b5ba399b08c68d99ae
Alihan Güdenoğlu <alihan_135@hotmail.com>
2021-10-19 00:02:34 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("merhaba kodlama io")
+
+let konutKredileri=["Taşıt Kredisi","Konut Kredisi","İhtiyaç kredisi","Kamu Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
