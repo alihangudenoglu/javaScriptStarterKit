f2d6810685e04756bd0aaadb2ad08b05b2d4cf54
Alihan Güdenoğlu <alihan_135@hotmail.com>
2021-11-22 23:42:36 +0300

    4. Gün kamp sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -15,11 +16,23 @@ userService.add(user2)
 userService.getById(1)
 userService.list(1)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
 let customer = { id: 1, firstName: "Engin" }
 //prototyping
 customer.lastName = "Demiroğ"
 
 //console.log(customer.lastName)
+
+console.log("-------------------")
+userService.load()
+
+let customerToAdd=new Customer(1,"alihan","güdenoğlu","eskişehir","sadsadsad")
+customerToAdd.type="customer"
+userService.add(customerToAdd)
+
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
